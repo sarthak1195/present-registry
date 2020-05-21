c4804c7d955d5ab802374bd819ec5620c7ee1831
Sarthak Gupta <43256009+sarthak1195@users.noreply.github.com>
2020-05-21 11:12:14 -0300

    Removing sensitive information from settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 #/usr/bin/env python
 APP_HOST = 'info3103.cs.unb.ca'
-APP_PORT = 8023
+APP_PORT = <!INSERT_PORT_HERE!>
 APP_DEBUG = True
 
 DB_HOST = 'localhost'
-DB_USER = 'jpontes'
-DB_PASSWD = 'SK7ES0lU'
+DB_USER = <!INSERT_USER_HERE!>
+DB_PASSWD = <!INSERT_PASSWORD_HERE!>
 DB_DATABASE = DB_USER
 
-SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+SECRET_KEY = <!ENTER_LDAP_SECRET_KEY_HERE!>
 
 LDAP_HOST = 'ldap-student.cs.unb.ca'
